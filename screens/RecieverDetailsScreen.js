8df0b40e7e1901ce4e1d8c8d4289b26cff4445a9
priyapandey2020 <priya.pandey@whitehatjr.com>
2020-10-23 16:54:10 +0530

    ..

---
+++ screens/RecieverDetailsScreen.js
@@ -0,0 +1,16 @@
+import React ,{Component} from 'react';
+import {View,Text} from 'react-native';
+
+export default class RecieverDetailsScreen extends Component{
+
+  render(){
+    return(
+      <View>
+        <text>
+          reciever details screen
+        </text>
+      </View>
+    )
+  }
+
+}
